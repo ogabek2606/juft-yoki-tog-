4ed2cb72be9da19160d69a98a64ad9270045e1f9
ogabek2606 <110012533+ogabek2606@users.noreply.github.com>
2022-07-30 03:22:59 -0400

    Add files via upload

---
+++ og`abek100/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int n = 10;
+
+
+        if (n % 2 == 0) {
+            System.out.println("juft");
+        }
+
+        if (n % 2 != 0) {
+            System.out.println("toq");
+        }
+
+    }
+}
